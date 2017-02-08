cd23b3c574c32b6bc0f62fa99d9f672a5f0c9cf2
Calvin <leather@usc.edu>
2017-02-07 21:02:46 -0800

    fixed order desc

--- app/Http/Controllers/index_controller.php
+++ app/Http/Controllers/index_controller.php
@@ -9,7 +9,7 @@ class index_controller extends Controller
     //
     public function __invoke()
     {
-      $results = \DB::select('select * from tweets;');
+      $results = \DB::select('select * from tweets ORDER BY id DESC;');
       return view('index', ['tweets'=>$results]);
     }
 }
